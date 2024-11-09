fa5adeefc41bec9736932680fa161b91459cd7dc
ardszsantos <lokgamer01@gmail.com>
2024-11-09 13:56:36 -0300

    feat: created the hello branch

--- hello.go
+++ hello.go
@@ -6,17 +6,6 @@ import (
 
 func main() {
 
-	nome := "Douglas"
-	idade := 24
+	fmt.Println("Hello World")
 
-	fmt.Println("Olá sr.", nome, "sua idade é ", idade)
-
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir os logs")
-	fmt.Println("0- Sair do Programa")
-
-	var comando int
-
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi", comando)
 }

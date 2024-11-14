1c8034ad328fcb162b39904820bbf19e01794f7e
ardszsantos <lokgamer01@gmail.com>
2024-11-14 14:19:10 -0300

    course and app finished

--- menu.go
+++ menu.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -14,8 +16,10 @@ const monitoramentos = 3
 const delay = 5
 
 func main() {
-	readFileSites()
 	exibeIntroducao()
+
+	registraLog("site-false", false)
+
 	for {
 
 		exibeMenu()
@@ -27,6 +31,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs....")
+			imprimeLogs()
 		case 3:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -90,8 +95,10 @@ func testaSite(site string) {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site: ", site, "foi carregado corretamente")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site: ", site, "está com um problema. Status Code:", resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
@@ -119,4 +126,29 @@ func readFileSites() []string {
 	arquivo.Close()
 
 	return sites
+}
+
+func registraLog(site string, status bool) {
+
+	arquivo, err := os.OpenFile("log.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05 ") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
+
 }
